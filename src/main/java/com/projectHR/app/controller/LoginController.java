6717f3f0713a72b2c4e433f22efb7bfde39a9f21
alexKamo <sashakamor@gmail.com>
2024-10-09 23:09:50 -0500

    upd

--- src/main/java/com/projectHR/app/controller/LoginController.java
+++ src/main/java/com/projectHR/app/controller/LoginController.java
@@ -1,4 +1,4 @@
-package com.projectHR.app.dao.controller;
+package com.projectHR.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
