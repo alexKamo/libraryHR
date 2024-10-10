e60d0a57e8dfba60be7a612a4562e737fabd3c8f
alexKamo <sashakamor@gmail.com>
2024-10-09 22:58:11 -0500

    upd

---
+++ src/main/java/com/projectHR/app/dao/controller/LoginController.java
@@ -0,0 +1,14 @@
+package com.projectHR.app.dao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showMyLoginPage")
+    public String showLoginPage(){
+        return "plain-login";
+    }
+
+}
