4fada526f1138ffaa638033f045d90e0cbad170a
alexKamo <sashakamor@gmail.com>
2024-10-26 14:51:00 -0500

    upd

--- src/main/java/com/projectHR/app/controller/UserController.java
+++ src/main/java/com/projectHR/app/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.projectHR.app.controller;
 
-import com.projectHR.app.entity.Employee;
 import com.projectHR.app.entity.HR_User;
 import com.projectHR.app.service.HRUserService;
 import org.springframework.stereotype.Controller;
@@ -26,7 +25,7 @@ public class UserController {
         List<HR_User> theUser = hrUserService.findUser();
 
         // add to the spring model
-        theModel.addAttribute("hr_user", theUser);
+        theModel.addAttribute("users", theUser);
 
         return "users/list-users";
     }
@@ -37,28 +36,28 @@ public class UserController {
         // create model attribute to bind form data
         HR_User theUser = new HR_User();
 
-        theModel.addAttribute("hr_user", theUser);
+        theModel.addAttribute("user", theUser);
 
         return "users/user-form";
     }
 
 
     @PostMapping("/showFormForUpdate")
-    public String showFormForUpdate(@RequestParam("hr_userId") int theId,
+    public String showFormForUpdate(@RequestParam("userId") int theId,
                                     Model theModel) {
 
         // get the employee from the service
         HR_User theUser = hrUserService.findByIdHrUser(theId);
 
         // set user as a model attribute to pre-populate the form
-        theModel.addAttribute("hr_user", theUser);
+        theModel.addAttribute("user", theUser);
 
         // send over to our form
         return "users/user-form";
     }
 
     @PostMapping("/save")
-    public String saveUser(@ModelAttribute("hr_user") HR_User theUser) {
+    public String saveUser(@ModelAttribute("user") HR_User theUser) {
 
         // save the user
         hrUserService.saveHrUser(theUser);
@@ -68,7 +67,7 @@ public class UserController {
     }
 
     @PostMapping("/delete")
-    public String delete(@RequestParam("hr_userId") int theId) {
+    public String delete(@RequestParam("userId") int theId) {
 
         // delete the user
         hrUserService.deleteByIdHrUser(theId);
