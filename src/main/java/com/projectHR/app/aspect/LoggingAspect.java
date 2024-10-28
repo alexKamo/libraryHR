ad96c336aceb6e72a38befec3b6b0c1daeea0085
alexKamo <sashakamor@gmail.com>
2024-10-28 18:38:35 -0500

    upd

--- src/main/java/com/projectHR/app/aspect/LoggingAspect.java
+++ src/main/java/com/projectHR/app/aspect/LoggingAspect.java
@@ -1,6 +1,9 @@
 package com.projectHR.app.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 import java.util.logging.Logger;
@@ -10,5 +13,26 @@ import java.util.logging.Logger;
 public class LoggingAspect {
 
     private Logger logger = Logger.getLogger(getClass().getName());
+
+    // setup pointcut declaration for controller package every method
+    // forControllerPackage()
+
+    @Pointcut("execution(* com.projectHR.app.controller.*.*(..))")
+    private void forControllerPackage(){}
+
+    @Pointcut("execution(* com.projectHR.app.service.*.*(..))")
+    private void forServicePackage(){}
+
+    @Pointcut("execution(* com.projectHR.app.dao.*.*(..))")
+    private void forDaoPackage(){}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow(){}
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint){
+        logger.info("Logging message for method: " + joinPoint.getSignature().toShortString());
+
+    }
 
 }
