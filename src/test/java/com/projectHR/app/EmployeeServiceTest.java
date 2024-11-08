ff3ac3b07b067378903152dab65734509568b6fc
alexKamo <sashakamor@gmail.com>
2024-11-07 21:09:06 -0600

    upd

--- src/test/java/com/projectHR/app/EmployeeServiceTest.java
+++ src/test/java/com/projectHR/app/EmployeeServiceTest.java
@@ -4,9 +4,17 @@ import com.projectHR.app.dao.EmployeeDAO;
 import com.projectHR.app.entity.Employee;
 import com.projectHR.app.service.EmployeeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class EmployeeServiceTest {
 
@@ -24,6 +32,8 @@ public class EmployeeServiceTest {
 
 
     // TEST findAll();
+
+
     // TEST findById();
     // TEST findById when exception throw
     // TEST save employee;
